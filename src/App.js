d6e5c7115cb5cd6952e64f949ce78d8999d9a84f
roboRiot <igor.a.sav@gmail.com>
2020-10-16 08:14:29 -0700

    added a lot of the firebase storage functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'
+import  firebase from './firebase'
+
+// import firebase from 'firebase/app';
+// import 'firebase/storage';
 
 function App() {
+  // var storage = require('@google-cloud/storage')
+  var storage = firebase.storage()
+  var ctr = 1
+
+  // var storeRef = storage.ref('abbistar/abbistar_1.jpg')
+  storage.ref('abbistar').listAll().then(function(result) {
+    result.items.forEach(function(imageRef){
+      
+      imageRef.getMetadata().then(function(result) {
+        // console.log(result["contentType"])
+
+        if(result["contentType"] == "application/json"){
+          displayJson(imageRef)
+        }
+        else{
+          displayImage(imageRef)
+        }
+      })
+
+
+      // displayImage(imageRef)
+    })
+  })
+
+  function displayJson(jsonRef){
+    jsonRef.getDownloadURL().then((url) => {
+      
+      $.get(url, function(response) {
+        console.log(response)
+      })
+      
+    })
+  }
+
+  function displayImage(imageRef){
+    imageRef.getDownloadURL().then((url) => {
+      document.getElementById(ctr).src=url
+      ctr = ctr + 1
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id='1' height="500" width="400"></img>
+      <img id='2' height="500" width="400"></img>
+      <a id = '3'></a>
+      
     </div>
   );
 }
---
+++ src/firebase/index.js
@@ -0,0 +1,24 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC86jZd_diTiPoZfBCCtPCGC5VEQUbrIKk",
+    authDomain: "rate-web.firebaseapp.com",
+    databaseURL: "https://rate-web.firebaseio.com",
+    projectId: "rate-web",
+    storageBucket: "rate-web.appspot.com",
+    messagingSenderId: "301893443708",
+    appId: "1:301893443708:web:d0d926494f41011cee69f9",
+    measurementId: "G-0EJ2S9BXBK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//   const storage = firebase.storage();
+
+//   var storeRef = storage.ref()
+
+  export {
+    //   storage, firebase as default
+    firebase as default
+  }
